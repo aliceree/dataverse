9a43e821e260cee3f9028a2afeffbc31dfa1e23b
aliceree <al.borovickova@gmail.com>
2025-09-23 20:37:27 +0200

    after first lesson

--- slicing.py
+++ slicing.py
@@ -1,4 +1,11 @@
-inzerat = "Na této pracovní pozoici budete používat Python."
-# Je "Python" v řetezci inzerat?
-if "Python" in inzerat:
-    print("Je to pro nás zajímavé")
+hodnoty = '12.1 1.68 7.45 -11.51'
+
+list_hodnoty = hodnoty.split()
+posledni_hodnota = list_hodnoty[-1]
+posledni_hodnota = float(posledni_hodnota)
+vysledek = posledni_hodnota + 0.25
+print(vysledek)
+vysledek = str(vysledek)
+list_hodnoty[-1] = vysledek
+vysledek_hodnoty = " ".join(list_hodnoty)
+print(vysledek_hodnoty)
