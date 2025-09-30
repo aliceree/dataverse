030264660e8272acbcf71004eebca9687e86de86
aliceree <al.borovickova@gmail.com>
2025-09-30 20:00:22 +0200

    Update JopRelatedFolder.py

--- JopRelatedFolder.py
+++ JopRelatedFolder.py
@@ -1,25 +1,9 @@
-# Zaokrouhlí číslo 12.34567 na celé číslo
-result = round(12.34567)  
-print(result)  # Výsledek bude 12
-
-# Definice funkce, která umí vydělit dvě čísla
-def divide_two_numbers(a, b):
-    # Podmínka: pokud jmenovatel b není nula
-    if b != 0:
-        result = a / b  # vydělíme číslo a číslem b
-        return result   # vrátíme výsledek
+def mile_na_kilometry(mile, namorni=False):
+    if not namorni:
+        return mile * 1.609344
     else:
-        # Pokud je b == 0, nelze dělit (matematicky nesmysl)
-        return "Nulou nelze dělit"
-
-# --- Použití funkce ---
-
-# Zavoláme funkci s hodnotami a=120, b=4
-# Podmínka b != 0 je splněna → proběhne dělení 120 / 4
-division_result = divide_two_numbers(120, 4)
-print(division_result)  # Výsledek bude 30.0 (float)
-
-# Zavoláme funkci s hodnotami a=120, b=0
-# Podmínka b != 0 není splněna → spustí se větev else
-division_result_2 = divide_two_numbers(120, 0)
-print(division_result_2)  # Vypíše "Nulou nelze dělit"
+        return mile * 1.852
+london_oxford_km = mile_na_kilometry(59.7)
+print(london_oxford_km)
+belfast_new_york = mile_na_kilometry(2758.13, True)
+print(belfast_new_york)
