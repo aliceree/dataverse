d81a6835b2b457cc892cf0b44dfdab241148474e
aliceree <al.borovickova@gmail.com>
2025-09-30 20:20:28 +0200

    Update JopRelatedFolder.py

--- JopRelatedFolder.py
+++ JopRelatedFolder.py
@@ -1,9 +1,7 @@
-def mile_na_kilometry(mile, namorni=False):
+def mile_na_kilometry(mile, namorni: bool=False):
+    # 0, "", [] se berou jako False
+    # neprázdný řetězec, neprázdný seznam, nenulové číslo se berou jako True
     if not namorni:
         return mile * 1.609344
     else:
         return mile * 1.852
-london_oxford_km = mile_na_kilometry(59.7)
-print(london_oxford_km)
-belfast_new_york = mile_na_kilometry(2758.13, True)
-print(belfast_new_york)
