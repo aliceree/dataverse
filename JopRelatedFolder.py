791b4913200a12f7cb8743f30758916ba03b4432
aliceree <al.borovickova@gmail.com>
2025-10-05 13:20:36 +0200

    Update JopRelatedFolder.py

--- JopRelatedFolder.py
+++ JopRelatedFolder.py
@@ -1,3 +1,11 @@
+pohyby = [1200, -250, -800, 540, 721, -613, -222]
+print(pohyby[2])
+print(pohyby[2:])
+print(len(pohyby))
+print(min(pohyby))
+print(max(pohyby))
+print (sum(pohyby))
+
 def mile_na_kilometry(mile, namorni: bool=False):
     # 0, "", [] se berou jako False
     # neprázdný řetězec, neprázdný seznam, nenulové číslo se berou jako True
