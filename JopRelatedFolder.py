7a0ed790c084dbd8e5e0403419aa15a310e254bc
aliceree <al.borovickova@gmail.com>
2025-10-06 09:35:20 +0200

    Update JopRelatedFolder.py

--- JopRelatedFolder.py
+++ JopRelatedFolder.py
@@ -1,4 +1,11 @@
-def kilometry_na_mile(kilometry):
-    return kilometry * 0.621
+# fuknce month_of_birth s parametrem birth_number
+# cílem je vrátit měsíc narození
+# birth_number = 9207054439
 
-print(f'10 mile je {kilometry_na_mile(10)} kilometru.')
+def month_of_birth(birth_number):
+    month = int(str(birth_number)[2:4])
+    if month > 50:
+        month - 50
+    return month
+
+print(month_of_birth(9207054439))
