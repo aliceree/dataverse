bb855c49d751d77492e7467d6a3f7b4a347ee232
aliceree <al.borovickova@gmail.com>
2025-10-07 19:17:23 +0200

    Update JopRelatedFolder.py

--- JopRelatedFolder.py
+++ JopRelatedFolder.py
@@ -1,6 +1,35 @@
-km = int(input("Zadej vzdálenost v km: "))
+'''
+**příklad:** V následujícím slovníku jsou uložena čísla lístků tomboly a příslušné výhry.
 
-def km_na_m(km):
-    return km / 0.001
+tombola = {
+    7: "Láhev kvalitního vína Château Headache",
+    15: "Pytel brambor z místního družstva",
+    23: "Čokoládový dort",
+    47: "Kniha o historii města",
+    55: "Šiška salámu",
+    67: "Vyhlídkový let balónem",
+    79: "Moderní televizor",
+    91: "Roční předplatné městského zpravodaje",
+    93: "Společenská hra Sázky a dostihy",
+}
 
-print(int(km_na_m(km)))
+- Napiš program, který se nejprve zeptá uživatele na číslo jeho lístku. Vstup uživatele si převeď na int!
+- Zkontroluj, zda je číslo lístku ve slovníku. Pokud ne, vypiš text "Bohužel nevyhráváš nic." Pokud číslo ve slovníku je, vypiš uživateli, co vyhrál.
+'''
+tombola = {
+    7: "Láhev kvalitního vína Château Headache",
+    15: "Pytel brambor z místního družstva",
+    23: "Čokoládový dort",
+    47: "Kniha o historii města",
+    55: "Šiška salámu",
+    67: "Vyhlídkový let balónem",
+    79: "Moderní televizor",
+    91: "Roční předplatné městského zpravodaje",
+    93: "Společenská hra Sázky a dostihy",
+}
+
+cislo_listku = int(input("Jaké je Vaše číslo lístku? "))
+if cislo_listku in tombola:
+    print(f"Gratuluji! Vyhráváte {tombola[cislo_listku]}!")
+else:
+    print("Bohužel Vaše číslo není výherní.")
