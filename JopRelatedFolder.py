5752789bdbb2e3fc3534416d39df2c07f7555314
aliceree <al.borovickova@gmail.com>
2025-10-07 17:53:08 +0200

    Update JopRelatedFolder.py

--- JopRelatedFolder.py
+++ JopRelatedFolder.py
@@ -1,8 +1,8 @@
-a = int(input("Největší hodnota je: "))
-b = int(input("Nejmenší hodnota je: "))
-result = a - b
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
 
-if result < 0:
-    print("Chyba: Největší hodnota musí být větší než nejmenší hodnota.")
-else:
-    print("Rozpětí je: ", result)
+def mult(a, b):
+    result = a * b
+    return result
+
+print(mult(a, b))
