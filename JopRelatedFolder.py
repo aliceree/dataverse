df01ce20e6f2ff713c896faf5a0d878968c6dd60
aliceree <al.borovickova@gmail.com>
2025-10-21 20:35:11 +0200

    PyLesson

--- JopRelatedFolder.py
+++ JopRelatedFolder.py
@@ -1,40 +1,32 @@
-class Package:
-    def __init__(self, address, weight, state):
-        self.address = address
-        self.weight = weight
-        self.state = state
+from dataclasses import dataclass
+
+@dataclass
+class Employee:
+    name: str
+    position: str
+    holiday_entitlement: int
 
     def __str__(self):
-        return f"Balík na adresu {self.address} má hmotnost {self.weight} kg je ve stavu {self.state}"
-    
-    def delivery_price(self):
-        if self.weight < 10:
-            return 129
-        elif self.weight <= 20:
-            return 159
+        return f"Zaměstnanec {self.name} pracuje na pozici {self.position}."
+   
+    def take_holiday(self, days):
+        if self.holiday_entitlement >= days:
+            self.holiday_entitlement -= days
+            return "Schváleno"
         else:
-            return 359
-        
-    def deliver(self):
-        if self.state == "dorucen":
-            return "Balík již byl doručen"
-        elif self.state == "nedorucen":
-            self.state = "dorucen"
-            return "Doručení uloženo"
+            return "Na tolik dní nemáš nárok"
 
-class ValuablePackage(Package):
-    def __init__(self, address, weight, state, value):
-        super().__init__(address, weight, state)
-        self.value = value
+@dataclass        
+class Manager(Employee):
+    subordinates: int
+    car: str
 
     def __str__(self):
         text = super().__str__()
-        return f"{text} Cena tohoto balíku je {self.value} Kč."
-    
-    def delivery_price(self):
-        price = super().delivery_price() * 1.05
-        return f"Cena balíku s pojištěním je {price} Kč."
+        return f"{text} Má {self.subordinates} podřízené."
 
-balik = Package ("U potoka 134/12", 25.0, "nedorucen")
-lepsi_balik = ValuablePackage ("U potoka 134/12", 25.0, "nedorucen", 10)
-print(lepsi_balik.delivery_price())
+frantisek = Employee("František Novák", "konstruktér", 25)
+klara = Employee("Klára Nová", "konstruktérka", 25)
+marian = Manager("Marian Přísný", "vedoucí", 25, 2, "Škoda Octavia")
+print(frantisek.take_holiday(20))
+print(marian)
--- notes/Python Inheritance.md
+++ notes/Python Inheritance.md
@@ -133,3 +133,55 @@ print(lepsi_balik.delivery_price())
 - Vytvořte jízdenku na vlak se základní cenou 150 do tříd economy i business. Zkontrolujte, jakou hodnotu vrací metoda `get_price()`.
 - Vytvoř letenku se základní cenou 6000 do tříd economy i business s jedním odbaveným zavazadlem. Zkontrolujte, jakou hodnotu vrací metoda `get_price()`.
 Vyzkoušejte vypočítat celkovou cenu dvou jízdenek různého typu, tj. jedné letenky a jedné jízdenky na vlak. Celkovou cenu uložte do proměnné `total_price` a k výpočtu použijte metodu `get_price()`.
+
+```python
+class Ticket: # jízdy autobusem
+    def __init__(self, basic_price, seat_number):
+        self.basic_price = basic_price
+        self.seat_number = seat_number
+    
+    class TrainTicket(Ticket):
+        def __init__(self, basic_price, seat_number, fare_class):
+            super
+```
+
+# datové třídy
+- `dataclass` si vytvoří obsah metody samy
+
+```python
+from dataclasses import dataclass
+
+@dataclass
+class Employee:
+    name: str
+    position: str
+    holiday_entitlement: int
+
+    def __str__(self):
+        return f"Zaměstnanec {self.name} pracuje na pozici {self.position}."
+   
+    def take_holiday(self, days):
+        if self.holiday_entitlement >= days:
+            self.holiday_entitlement -= days
+            return "Schváleno"
+        else:
+            return "Na tolik dní nemáš nárok"
+
+@dataclass        
+class Manager(Employee):
+    subordinates: int
+    car: str
+
+    def __str__(self):
+        text = super().__str__()
+        return f"{text} Má {self.subordinates} podřízené."
+
+frantisek = Employee("František Novák", "konstruktér", 25)
+klara = Employee("Klára Nová", "konstruktérka", 25)
+marian = Manager("Marian Přísný", "vedoucí", 25, 2, "Škoda Octavia")
+print(frantisek.take_holiday(20))
+print(marian)
+```
+
+## abstraktní třídy
+- 
