a54215e63f6a62a929cf1405f55577ab370d7c9a
aliceree <al.borovickova@gmail.com>
2025-11-04 18:27:19 +0100

    PyLesson 6

--- JopRelatedFolder.py
+++ JopRelatedFolder.py
@@ -1,33 +1,19 @@
-class Car:
-    def __init__(self, SPZ, car_brand, miles_driven, availability=True):
-        self.SPZ = SPZ
-        self.car_brand = car_brand
-        self.miles_driven = miles_driven
-        self.availability = availability
-    
-    def rent_car(self):
-        if self.availability == True:
-            self.availability = False
-            return f"Potvrzuji zapůjčení vozidla."
-        else:
-            return f"Vozidlo není k dispozici."
+class Employee:
+    def __init__(self, name: str, position, holiday_entitlement):
+        self.name = name
+        self.position = position
+        self.holiday_entitlement = holiday_entitlement
         
-    def __str__(self):
-        return f"Automobil {self.car_brand} má SPZ {self.SPZ} a je {'k dispozici' if self.availability else 'nedostupný'}."
-    
-def user_input():
-    if user_choice == Car1.car_brand:
-        print(Car1.rent_car())
-    elif user_choice == Car2.car_brand:
-        print(Car2.rent_car())
-    else:
-        print("Taková značka není k dispozici.")
-    
-user_choice = input("Zadejte značku vozidla, které si přeješ vypůjčit: ")
+class Manager(Employee):
+    def __init__(self, name: str, position, holiday_entitlement, subordinates, car):
+        super().__init__(name, position, holiday_entitlement)
+        self.subordinates = subordinates
+        self.car = car
 
-Car1 = Car("4A2 3020", "Peugeot", 47534, True)
-Car2 = Car("1P3 4747", "Škoda", 41253, True)
+marian = Manager("Marian", "vedoucí konstrukčního oddělení", 25, 5, "Škoda Octavia")
+frantisek = Employee("František", "konstruktér", 20)
 
-user_input()
-print(Car2)
-print(Car1)
+if isinstance(frantisek, Manager):
+    print(f"{frantisek.name} pochází ze třídy Manager.")
+else:
+    print(f"{frantisek.name} nepochází ze třídy Manager.")
