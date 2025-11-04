85e01deba9ecf8f4a42aee806d9a8e18ffafbe1f
aliceree <al.borovickova@gmail.com>
2025-11-04 18:00:41 +0100

    pre-lesson review

--- JopRelatedFolder.py
+++ JopRelatedFolder.py
@@ -1,32 +1,33 @@
-from dataclasses import dataclass
-
-@dataclass
-class Employee:
-    name: str
-    position: str
-    holiday_entitlement: int
-
-    def __str__(self):
-        return f"Zaměstnanec {self.name} pracuje na pozici {self.position}."
-   
-    def take_holiday(self, days):
-        if self.holiday_entitlement >= days:
-            self.holiday_entitlement -= days
-            return "Schváleno"
+class Car:
+    def __init__(self, SPZ, car_brand, miles_driven, availability=True):
+        self.SPZ = SPZ
+        self.car_brand = car_brand
+        self.miles_driven = miles_driven
+        self.availability = availability
+    
+    def rent_car(self):
+        if self.availability == True:
+            self.availability = False
+            return f"Potvrzuji zapůjčení vozidla."
         else:
-            return "Na tolik dní nemáš nárok"
-
-@dataclass        
-class Manager(Employee):
-    subordinates: int
-    car: str
-
+            return f"Vozidlo není k dispozici."
+        
     def __str__(self):
-        text = super().__str__()
-        return f"{text} Má {self.subordinates} podřízené."
+        return f"Automobil {self.car_brand} má SPZ {self.SPZ} a je {'k dispozici' if self.availability else 'nedostupný'}."
+    
+def user_input():
+    if user_choice == Car1.car_brand:
+        print(Car1.rent_car())
+    elif user_choice == Car2.car_brand:
+        print(Car2.rent_car())
+    else:
+        print("Taková značka není k dispozici.")
+    
+user_choice = input("Zadejte značku vozidla, které si přeješ vypůjčit: ")
 
-frantisek = Employee("František Novák", "konstruktér", 25)
-klara = Employee("Klára Nová", "konstruktérka", 25)
-marian = Manager("Marian Přísný", "vedoucí", 25, 2, "Škoda Octavia")
-print(frantisek.take_holiday(20))
-print(marian)
+Car1 = Car("4A2 3020", "Peugeot", 47534, True)
+Car2 = Car("1P3 4747", "Škoda", 41253, True)
+
+user_input()
+print(Car2)
+print(Car1)
--- notes/Python Objects and Classes.md
+++ notes/Python Objects and Classes.md
@@ -225,7 +225,39 @@ Vytvořte funkci `__init__` pro třídu `Car`. Registrační značku, značku a 
 Vytvořte objekty, které reprezentují všechny automobily půjčovny.
 
 ```python
-class Car():
+class Car:
+    def __init__(self, SPZ, car_brand, miles_driven, availability=True):
+        self.SPZ = SPZ
+        self.car_brand = car_brand
+        self.miles_driven = miles_driven
+        self.availability = availability
+    
+    def rent_car(self):
+        if self.availability == True:
+            self.availability = False
+            return f"Potvrzuji zapůjčení vozidla."
+        else:
+            return f"Vozidlo není k dispozici."
+        
+    def __str__(self):
+        return f"Automobil {self.car_brand} má SPZ {self.SPZ} a je {'k dispozici' if self.availability else 'nedostupný'}."
+    
+def user_input():
+    if user_choice == Car1.car_brand:
+        print(Car1.rent_car())
+    elif user_choice == Car2.car_brand:
+        print(Car2.rent_car())
+    else:
+        print("Taková značka není k dispozici.")
+    
+user_choice = input("Zadejte značku vozidla, které si přeješ vypůjčit: ")
+
+Car1 = Car("4A2 3020", "Peugeot", 47534, True)
+Car2 = Car("1P3 4747", "Škoda", 41253, True)
+
+user_input()
+print(Car2)
+print(Car1)
 ```
 
 zdroj a cvičení: https://kodim.cz/czechitas/uvod-do-progr-2/uvod-do-programovani-2/tridy/tridy
