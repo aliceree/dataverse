13d92e24959768c2e1a8fa532936323b933bb03f
aliceree <al.borovickova@gmail.com>
2025-11-18 20:17:01 +0100

    PyLesson

--- JopRelatedFolder.py
+++ JopRelatedFolder.py
@@ -1,17 +1,5 @@
-lines = [
-    "2904,4",
-    "7390,7",
-    "6950,8",
-    "3300,4",
-    "10570,8",
-    "1310,2",
-    "9806,8"
-]
+import humanize
+number = 1234567890876543234567898734567654
+print(humanize.intword(number))
 
-avg_sales = []
-for line in lines:
-    line = line.split(",")
-    avg = int(line[0]) / int(line[1])
-    avg_sales.append(avg)
-
-print(avg_sales)
+print(humanize.apnumber(23))
